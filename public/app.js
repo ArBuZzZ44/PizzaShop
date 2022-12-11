3338d62f9a94cbd075ea2d7410744357c769c54c
ArBuZzZ44 <zahar1702@mail.ru>
2022-12-11 22:33:06 +0700

    add get_quantity_cart function

--- public/app.js
+++ public/app.js
@@ -19,4 +19,22 @@ function something()
 		x = x * 1 + 1;
 		localStorage.setItem(key, x);
 
+		alert("There are " + get_cart_quantity() + " pizzas in your cart");
  	}
+
+function get_cart_quantity()
+	{
+		var cnt = 0;
+
+		for(var i = 0; i < localStorage.length; i++)
+		{
+			var key = localStorage.key(i); // получаем ключ
+			var value = localStorage.getItem(key); //получаем значение
+
+			if(key.indexOf("product_") == 0)
+			{
+				cnt = cnt + value * 1;
+			}
+		}
+		return cnt;
+	}
