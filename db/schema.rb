0d7d76092d262e8fe972265733d1feda288491cf
ArBuZzZ44 <zahar1702@mail.ru>
2022-12-15 23:04:58 +0700

    create migration order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_09_193820) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_160230) do
+  create_table "orders", force: :cascade do |t|
+    t.text "name"
+    t.text "phone"
+    t.text "address"
+    t.text "orders_input"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
